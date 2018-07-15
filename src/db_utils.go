f90844ca17d28264bb955fa9655b1fb2eade4b2f
Thibaud Bonnefoy <thibaud.bonnefoy@epitech.eu>
2018-07-15 12:51:12 +0200

    new openDatabase func in db_utils.go -> open database connection

--- src/db_utils.go
+++ src/db_utils.go
@@ -1,8 +1,31 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"fmt"
+)
+
 type db_login struct {
 	Adress		string		`json:"address"`
 	Username	string		`json:"username"`
 	Password	string		`json:"password"`
 	Database	string		`json:"database"`
+}
+
+func openDatabase(opt Options) *sql.DB {
+	dblogin := get_login(opt)
+	connectionStr := fmt.Sprintf("%s:%s@%s/%s",
+			dblogin.Username,
+			dblogin.Password,
+			dblogin.Adress,
+			dblogin.Database)
+	var db *sql.DB
+	var err error
+
+	db, err = sql.Open("mysql", connectionStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
