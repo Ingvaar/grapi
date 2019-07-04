f67ae60d629b40affd713fb44d6e80b682146ea6
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2019-07-04 23:02:29 +0200

    adding tests to get_config

--- config/get_config_test.go
+++ config/get_config_test.go
@@ -1,9 +1,34 @@
 package config
 
 import (
+	"io/ioutil"
+	"log"
+	"strings"
 	"testing"
+
+	"grapi/core"
 )
 
-func TestGetConfig(t *testing.T) {
+func TestFileParsing(t *testing.T) {
+	config := new(core.Config)
+	log.SetOutput(ioutil.Discard)
 
+	t.Run("parsJsonOK", func(t *testing.T) {
+		err := parsFile(strings.NewReader("{\"cache\": 0}"), config)
+		if err != nil {
+			t.Errorf("JSON parsing error: %v", err)
+		}
+	})
+	t.Run("parsJsonKO", func(t *testing.T) {
+		err := parsFile(strings.NewReader("\"che\" 0}"), config)
+		if err == nil {
+			t.Errorf("JSON error detecting error")
+		}
+	})
+	t.Run("readerKO", func(t *testing.T) {
+		err := parsFile(strings.NewReader("{\"cache\": 0}"), config)
+		if err == nil {
+			t.Errorf("parsFile reader error")
+		}
+	})
 }
